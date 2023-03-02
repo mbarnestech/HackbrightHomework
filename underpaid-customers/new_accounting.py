30a112a34b0b556f77b828e9f28eabf1ba25c4b6
mbarnestech <maggie.barnes@gmail.com>
2023-03-02 16:48:39 -0700

    update code to add first name, indication of whether accound is underpaid, and how much is owed

--- underpaid-customers/new_accounting.py
+++ underpaid-customers/new_accounting.py
@@ -20,18 +20,29 @@ def run_report(melon_cost, file_name):
             melons = int(order_info[2])
             price_paid = float(order_info[3])
 
+            # get first name from customer name
+            first_name = customer.split(' ')[0]
+
             # expected cost is the number of melons times the cost per melon
             expected = melons * melon_cost
             
             # if expected is different than the price paid 
             if expected != price_paid:
 
+                # check who owes who how much
+                if expected < price_paid:
+                    status = 'overpaid'
+                    owe = f'We owe {first_name} ${price_paid-expected:.2f}.'
+                else:
+                    status = 'underpaid'
+                    owe = f'{first_name} owes us ${expected-price_paid:.2f}.'
+
                 # print a line explaining the situation
-                print(f"Order {order_num}: {customer} paid ${price_paid:.2f}. Expected ${expected:.2f}")
+                print(f"Order {order_num}: {customer} paid ${price_paid:.2f}. Expected ${expected:.2f}. {first_name} {status}. {owe} ")
     
     # this function returns 'None'
     return
                       
-                      
+
 # run the report for a melon cost of $1.00 on the customer orders file
 run_report(1, 'customer-orders.txt')
