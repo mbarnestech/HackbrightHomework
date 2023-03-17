b7bf2bb50b6f62bbc2cb5b5906ec69f456e66ad0
mbarnestech <maggie.barnes@gmail.com>
2023-03-16 21:36:52 -0600

    make all recommended changes

--- study-guide-week-3/coding-practice/server.py
+++ study-guide-week-3/coding-practice/server.py
@@ -5,6 +5,8 @@ from flask import Flask, render_template, session, request, redirect
 app = Flask(__name__)
 app.secret_key = "RANDOM SECRET KEY"
 
+MOODS = ['cheery', 'honest', 'dreary']
+
 @app.route('/')
 def show_homepage():
     """Show homepage."""
@@ -20,13 +22,24 @@ def show_homepage():
 @app.route('/form')
 def show_form():
     """Show form with message options."""
-    pass
+    return render_template('form.html', moods = MOODS)
 
 @app.route('/results')
 def show_results():
     """Show resulting message."""
-    pass
 
+    checked_moods = request.args.getlist('mood')
+
+    message = f'Have a {", ".join([mood for mood in checked_moods])} day'
+
+    return render_template('results.html', message = message)
+
+@app.route('/save-name')
+def save_name():
+
+    session['name'] = request.args.get('name')
+
+    return render_template('homepage.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
