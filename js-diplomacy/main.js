32a20d2d88760f0a48fab4fe9fe4557a53af83b4
mbarnestech <maggie.barnes@gmail.com>
2023-03-21 21:25:47 -0600

    add comments to code

--- js-diplomacy/main.js
+++ js-diplomacy/main.js
@@ -6,8 +6,11 @@ const colorChangerButton = document.querySelector('button.color-changer');
 // get all elements with color-change class
 const colorChangeClass = document.querySelectorAll('.color-change');
 
+// listen for the 'click' event on the color change button
 colorChangerButton.addEventListener('click', () => {
+    // loop through each element with color-change class
     for (const item of colorChangeClass) {
+        // add 'red' class to each element
         item.classList.add('red');
     }
 });
