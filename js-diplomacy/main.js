41b78a3bfb793a7eebc12dc9096b5376cdd09d0e
mbarnestech <maggie.barnes@gmail.com>
2023-03-21 21:28:16 -0600

    add task descriptions to code

--- js-diplomacy/main.js
+++ js-diplomacy/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// FIRST TASK: COLOR CHANGE BUTTON
+
 // get the button with color-changer class
 const colorChangerButton = document.querySelector('button.color-changer');
 
@@ -14,3 +16,11 @@ colorChangerButton.addEventListener('click', () => {
         item.classList.add('red');
     }
 });
+
+// SECOND TASK: "When someone submits the form, prevent its default behavior 
+    // (it should not redirect to a new page). Instead, validate the input in 
+    // the text input with ID “number-input”. If it is not a number or greater 
+    // than or equal to 10, change the text of the element with ID “formFeedback” 
+    // to say ‘Please enter a smaller number’. Otherwise, change the text of that 
+    // element to say “You are good to go!.”"
+
