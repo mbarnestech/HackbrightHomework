1997b315179c23e5f01b1b348e6cdcae1c73360c
mbarnestech <maggie.barnes@gmail.com>
2023-03-23 22:13:26 -0600

    add fetch method to make fortune and weather paths work

--- ajax-review/static/ajax-review.js
+++ ajax-review/static/ajax-review.js
@@ -1,7 +1,11 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune() {
-  // TODO: get the fortune and show it in the fortune-text div
+  fetch('/fortune')
+    .then((fortunePromise)=>fortunePromise.text())
+    .then((fortuneText)=>{
+      document.querySelector('#fortune-text').innerHTML = fortuneText;
+    })
 }
 
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
@@ -12,7 +16,12 @@ function showWeather(evt) {
   evt.preventDefault();
   const zipcode = document.querySelector('#zipcode-field').value;
   const url = `/weather?zipcode=${zipcode}`;
-  // TODO: request weather with that URL and show the forecast in #weather-info
+
+  fetch(url)
+    .then((response)=>response.json())
+    .then((weatherInfo)=>{
+      document.querySelector('#weather-info').innerHTML = weatherInfo.forecast;
+    })
 }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
